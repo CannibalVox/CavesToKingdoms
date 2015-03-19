911fc1691cceefdeff54909e669db12a15b5e53e
Talonos2 <Talonos@Gmail.com>
2015-03-19 14:18:50 -0600

    Get the file all cleaned up before maybe getting the courage to show it to CanVox.

--- src/main/java/talonos/blightbuster/ItemWorldTainter.java
+++ src/main/java/talonos/blightbuster/ItemWorldTainter.java
@@ -26,8 +26,10 @@ import net.minecraftforge.common.util.ForgeDirection;
 public class ItemWorldTainter extends Item
 {
 	
-	private Set<Block> toRandomlyReplace;
+	//All items in this set have a 50% chance of being replaced with Fibrous Taint when the world-tainter is used. 
+	private static Set<Block> toRandomlyReplace;
 	
+	//Boilerplate setup stuff.
 	public ItemWorldTainter() 
 	{
 		setUnlocalizedName(BlightBuster.MODID + "_" + BBStrings.worldTainterName);
@@ -39,54 +41,42 @@ public class ItemWorldTainter extends Item
 	@Override
     public ItemStack onItemRightClick(ItemStack itemStack, World theWorld, EntityPlayer thePlayer)
     {
+		//Lazily initialize the list of stuff to randomly replace.
 		if (toRandomlyReplace == null)
 		{
 			toRandomlyReplace = new HashSet<Block>();
 	        toRandomlyReplace.add(Blocks.snow_layer);
 	        toRandomlyReplace.add(GameRegistry.findBlock("Natura", "N Crops"));
 		}
+		
+		//Only do server-side
 		if (!theWorld.isRemote)
 		{
+			//Get the chunk the player is in (round down to the nearest multiple of 16)
 			int x = ((int)thePlayer.posX/16)*16;
 			int z = ((int)thePlayer.posZ/16)*16;
 			
+			//Inform the player.
 			thePlayer.addChatMessage(new ChatComponentText("Tainting and UBifying world based on coords: "));
 			thePlayer.addChatMessage(new ChatComponentText("  xSection: "+x+", zSection: "+z));
 			
+			//Taint an 11 chunk diameter area.
 			for (int xLoc = x-80; xLoc<x+96; xLoc++)
 			{
 				for (int zLoc = z-80; zLoc<z+96; zLoc++)
 				{
-					//System.out.println("Here");
+					//Dunno how this works. Thaumcraft's "Utils" package does all the real work. :/
 					Utils.setBiomeAt(theWorld, xLoc, zLoc, ThaumcraftWorldGenerator.biomeTaint);
-					/*for (int yLoc = 254; yLoc > 1; yLoc--)
-					{
-						if (theWorld.getBlock(xLoc, yLoc, zLoc).equals(Blocks.sand)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.air)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.water)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.flowing_water)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.sand)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.cactus)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.deadbush)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.glass)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.sandstone)&&
-						    !theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(ConfigBlocks.blockTaintFibres))
-						{
-							theWorld.setBlock(xLoc, yLoc, zLoc, Blocks.stone);
-						}
-						if (theWorld.getBlock(xLoc, yLoc, zLoc).equals(Blocks.sandstone)&&
-							theWorld.getBlock(xLoc, yLoc+1, zLoc).equals(Blocks.stone))
-							{
-								theWorld.setBlock(xLoc, yLoc, zLoc, Blocks.stone);
-							}
-					}*/
 					
+					//For each Y level...
 					for (int yLoc = 1; yLoc < 254; yLoc++)
 					{
+						//Replace Air blocks on solid surfaces with Taint blocks
 						if (theWorld.isSideSolid(xLoc, yLoc-1, zLoc, ForgeDirection.UP, false)&&theWorld.getBlock(xLoc, yLoc, zLoc)==Blocks.air)
 						{
 							theWorld.setBlock(xLoc, yLoc, zLoc, ConfigBlocks.blockTaintFibres);
 						}
+						//Maybe replace other blocks with taint blocks.
 						if (toRandomlyReplace.contains(theWorld.getBlock(xLoc, yLoc, zLoc)))
 						{
 							if (theWorld.rand.nextBoolean())
@@ -98,8 +88,9 @@ public class ItemWorldTainter extends Item
 				}
 			}
 			
-			thePlayer.addChatMessage(new ChatComponentText("  World should now be tainted."));
-			
+			thePlayer.addChatMessage(new ChatComponentText("  World should now be tainted.")); //Well it should. :/
+
+			//While we're at it, UBify all ores in the area around us as well.
 			for (int xLoc = x-80; xLoc<x+96; xLoc+=16)
 			{
 				for (int zLoc = z-80; zLoc<z+96; zLoc+=16)
@@ -110,8 +101,8 @@ public class ItemWorldTainter extends Item
 			
 			thePlayer.addChatMessage(new ChatComponentText("  Ores should now be fixed."));
 			
+			//I was told by Zeno112 that this should prevent memory leaks.
 			theWorld.getChunkProvider().unloadQueuedChunks();
-
 			IntCache.resetIntCache();
 		}
         
