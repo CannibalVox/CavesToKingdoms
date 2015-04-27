e8f7bf19a4bc2ef07dbfb86f1733a8478ef12ed2
CannibalVox <sdbaynham@gmail.com>
2015-04-27 11:47:26 -0500

    Badges look/act right now.

--- src/main/java/talonos/biomescanner/BSItems.java
+++ src/main/java/talonos/biomescanner/BSItems.java
@@ -1,13 +1,15 @@
 package talonos.biomescanner;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
+import talonos.biomescanner.item.ItemBadge;
 
 public class BSItems {
 
-	public static void init() 
-	{
-		badge = new Item().setUnlocalizedName("badge").setTextureName("badge");
-	}
+    public static void init() {
+        badge = new ItemBadge().setMaxStackSize(1);
+        GameRegistry.registerItem(badge, "biomescanner:badge");
+    }
 
-	public static Item badge;
+    public static Item badge;
 }
---
+++ src/main/java/talonos/biomescanner/item/ItemBadge.java
@@ -0,0 +1,98 @@
+package talonos.biomescanner.item;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
+import talonos.biomescanner.map.Zone;
+
+import java.util.List;
+
+public class ItemBadge extends Item {
+    private IIcon[] zoneIcons = new IIcon[Zone.values().length * 3];
+    private IIcon completionIcon;
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IIconRegister iconRegister) {
+        completionIcon = iconRegister.registerIcon("biomescanner:completionBadge");
+
+        for(Zone zone : Zone.values()) {
+            zoneIcons[zone.ordinal()*3] = iconRegister.registerIcon("biomescanner:zoneBadge-"+zone.toString()+"-bronze");
+            zoneIcons[zone.ordinal()*3 + 1] = iconRegister.registerIcon("biomescanner:zoneBadge-"+zone.toString()+"-silver");
+            zoneIcons[zone.ordinal()*3 + 2] = iconRegister.registerIcon("biomescanner:zoneBadge-"+zone.toString()+"-gold");
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIconFromDamage(int damage) {
+        if (damage < zoneIcons.length)
+            return zoneIcons[damage];
+        else
+            return completionIcon;
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack stack)
+    {
+        int damage = stack.getItemDamage();
+        if (damage >= zoneIcons.length)
+            return StatCollector.translateToLocal("item.completionBadge.name");
+
+        int medalType = damage % 3;
+
+        String zoneName = StatCollector.translateToLocal(Zone.values()[damage/3].getDisplay());
+        String medalDisplay;
+        switch (medalType) {
+            case 0:
+                medalDisplay = StatCollector.translateToLocal("item.zoneBronze.name");
+                break;
+            case 1:
+                medalDisplay = StatCollector.translateToLocal("item.zoneSilver.name");
+                break;
+            case 2:
+            default:
+                medalDisplay = StatCollector.translateToLocal("item.zoneGold.name");
+        }
+
+        return medalDisplay.replace("{0}", zoneName);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List info, boolean advanced) {
+        int damage = itemStack.getItemDamage();
+        if (damage < zoneIcons.length) {
+            String zoneName = StatCollector.translateToLocal(Zone.values()[damage/3].getDisplay());
+            int medalType = damage % 3;
+            switch (medalType) {
+                case 0:
+                    addBadgeData("gui.bronzeInfo", zoneName, info);
+                    break;
+                case 1:
+                    addBadgeData("gui.silverInfo", zoneName, info);
+                    break;
+                case 2:
+                    addBadgeData("gui.goldInfo", zoneName, info);
+                    break;
+            }
+            return;
+        }
+
+        addBadgeData("gui.completionInfo", "", info);
+    }
+
+    private void addBadgeData(String value, String zoneName, List info) {
+        for (int i = 0; i < 99; i++) {
+            if (!StatCollector.canTranslate(value + Integer.toString(i)))
+                break;
+
+            info.add("\u00a79" + StatCollector.translateToLocal(value + Integer.toString(i)).replace("{0}", zoneName));
+        }
+    }
+}
