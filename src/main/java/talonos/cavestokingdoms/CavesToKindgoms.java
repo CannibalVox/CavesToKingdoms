7611e6946e7e77cc098700da192ca2ea434d0567
CannibalVox <sdbaynham@gmail.com>
2015-05-06 16:54:25 -0500

    Forgot to remove this helper message when I dumped the materials

--- src/main/java/talonos/cavestokingdoms/CavesToKindgoms.java
+++ src/main/java/talonos/cavestokingdoms/CavesToKindgoms.java
@@ -37,31 +37,7 @@ public class CavesToKindgoms
     {
     	MinecraftForge.EVENT_BUS.register(new OrbEventHandler());
 
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("Soft1", "soft1", 0, 40, 150, 1, 1.0f, 0, 0, "\u00a7e", 0x755821));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("HardNonMetal", "hardnonmetal", 1, 80, 150, 1, 0.30000001192092896f, 0, 1, "\u00a77", 0x7F7F7F));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("SoftMetal", "softmetal", 2, 144, 500, 2, 1.100000023841858f, 0, 0, "\u00a7c", 0xCC6410));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("HardMetal", "hardmetal", 3, 200, 600, 3, 1.2000000476837158f, 1, 0, "\u00a7f", 0xDADADA));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("DiamondLevel", "diamondlevel", 4, 320, 700, 3, 1.3f, 2, 0, "\u00a77", 0xA0A0A0));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("SupraDiamond", "supradiamond", 5, 534, 780, 3, 1.35f, 1, 0, "\u00a7c", 0xF0A8A4));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("Alien", "alien", 6, 640, 1100, 4, 1.75f, 2, 0, "\u00a73", 0xFFFFFF));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("Amazing", "amazing", 7, 960, 900, 5, 2.5f, 0, 0, "\u00a75", 0x7338A5));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("Extreme", "extreme", 8, 1240, 1000, 5, 2.3f, 2, 0, "\u00a74", 0xFFFFFF));
-        TConstructRegistry.addtoolMaterial(getNextClearMaterialId(), new ToolMaterial("Epic", "epic", 9, 1600, 1500, 6, 3.33f, 0, 0, "\u00a76", 0xFFFFFF));
-
         manualInfo = new ManualInfo();
-    }
-
-    private static int getNextClearMaterialId() {
-        int id = 0;
-        while (id < 1024) {
-            ToolMaterial material = TConstructRegistry.getMaterial(id);
-
-            if (material == null)
-                return id;
-            id++;
-        }
-
-        throw new RuntimeException("All TCon material ID's are taken.  What is this sorcery?");
     }
  
     @Mod.EventHandler
