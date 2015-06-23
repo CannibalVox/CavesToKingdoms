414285a46504d3c7920ae162757cd42ce811b261
CannibalVox <sdbaynham@gmail.com>
2015-06-23 12:31:18 -0400

    Add cyber totem to the blocks creative tab

--- src/main/java/talonos/blightbuster/blocks/BlockCyberTotem.java
+++ src/main/java/talonos/blightbuster/blocks/BlockCyberTotem.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.IIcon;
 import talonos.blightbuster.BBStrings;
 import talonos.blightbuster.BlightBuster;
@@ -21,6 +22,7 @@ public class BlockCyberTotem extends BBBlock {
         this.setResistance(500.0F);
         this.setBlockName(BlightBuster.MODID + "_" + BBStrings.cyberTotemName);
         this.setStepSound(soundTypeWood);
+        this.setCreativeTab(CreativeTabs.tabBlock);
         GameRegistry.registerBlock(this, this.getUnlocalizedName());
     }
 
