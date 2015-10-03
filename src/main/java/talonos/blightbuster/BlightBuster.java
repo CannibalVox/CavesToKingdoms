6d4332929a8354b8d181383760d1042bee7c3c4d
CannibalVox <sdbaynham@gmail.com>
2015-10-03 18:39:58 -0400

    Set version correctly

--- src/main/java/talonos/blightbuster/BlightBuster.java
+++ src/main/java/talonos/blightbuster/BlightBuster.java
@@ -25,7 +25,7 @@ public class BlightBuster
 	
 	public static final String MODID = "blightbuster";
 	public static final String MODNAME = "BlightBuster";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.2.19";
     public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
 	public static final String COMMONPROXYLOCATION = "talonos."+MODID+".CommonProxy";
 	public static final String CLIENTPROXYLOCATION = "talonos."+MODID+".ClientProxy";
--- src/main/java/talonos/cavestokingdoms/lib/DEFS.java
+++ src/main/java/talonos/cavestokingdoms/lib/DEFS.java
@@ -4,7 +4,7 @@ public class DEFS
 {
     public static final String MODID = "cavestokingdoms";
     public static final String MODNAME = "Caves to Kingdoms";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.2.19";
     public static final String DEPS = "before:UndergroundBiomes;after:ThermalFoundation;after:appliedenergistics2;after:Thaumcraft";
 
     
